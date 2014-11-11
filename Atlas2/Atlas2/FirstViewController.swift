dc3f5177482e29a6b17534e95b6d6110c3492c1c
Milee00me <mish8160@colorado.edu>
2014-11-11 07:01:09 -0700

    Project 2

---
+++ Atlas2/Atlas2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Atlas2
+//
+//  Created by Milee Shrestha on 11/6/14.
+//  Copyright (c) 2014 Milee Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Atlas2/Atlas2/data.swift
@@ -0,0 +1,23 @@
+//
+//  data.swift
+//  Atlas2
+//
+//  Created by Milee Shrestha on 11/10/14.
+//  Copyright (c) 2014 Milee Shrestha. All rights reserved.
+//
+
+import Foundation
+
+class data {
+    
+    var firstCourse : String?
+    var secondCourse : String?
+    var thirdCourse : String?
+    
+    init(){
+        
+       firstCourse=nil
+       secondCourse=nil
+       thirdCourse=nil
+    }
+}
